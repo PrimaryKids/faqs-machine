04a2b9a0ba50fc10558f1eb43129e988d76d33e3
Corey Psoinos <cpsoinos@primary.com>
2020-01-24 13:43:34 -0500

    route to correct url to edit form

--- src/components/faqs/FaqList.js
+++ src/components/faqs/FaqList.js
@@ -62,7 +62,7 @@ const FaqList = props => {
           <svg className='faq-m-r-3' xmlns='http://www.w3.org/2000/svg' height='30' width='30' viewBox='20 20 60 60'><path d='M73,48.4l-10.4-9.6v4.8H52.4V33.4h4.8L47.6,23l-8.9,10.4h4.8v10.2H33.4v-4.8L23,48.4l10.4,8.9v-4.8h10.2v10.2h-4.8L47.6,73   l9.6-10.4h-4.8V52.4h10.2v4.8L73,48.4z' /></svg>
             <span className='faq-title--sm'>{faq.question}</span>
         </div>
-        <Link to={`/faq-groups/${faq.faqGroupId}/faqs/${faq.id}`} className='faq-text-link--primary'>Edit</Link>
+        <Link to={`/faq-groups/${faq.faqGroupId}/faqs/${faq.id}/edit`} className='faq-text-link--primary'>Edit</Link>
       </div>
     )
   }
