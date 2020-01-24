166497b58beaf1beb4365f178f17dbf1fd0fb694
Corey Psoinos <cpsoinos@primary.com>
2020-01-24 12:53:36 -0500

    persist auth state with sessionStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ToastProvider } from 'react-toast-notifications'
 import Header from './components/Header'
 import {
@@ -12,10 +12,20 @@ import './Faq.css'
 import FaqGroup from './components/FaqGroup'
 import GoogleLogin from 'react-google-login'
 import useGlobal from './store'
+import { useSessionStorageState } from 'react-storage-hooks'
 
 const App = () => {
   const [error, onFailure] = useState(null)
+  const [idToken, setIdToken] = useSessionStorageState()
   const [globalState, globalActions] = useGlobal()
+
+  useEffect(() => {
+    if (idToken) {
+      globalActions.setIdToken(idToken)
+      globalActions.setApiClient()
+      globalActions.setIsSignedIn(true)
+    }
+  }, [globalActions, idToken])
 
   const getContent = () => {
     if (globalState.isSignedIn) {
@@ -47,6 +57,7 @@ const App = () => {
   }
 
   const onSuccess = response => {
+    setIdToken(response.tokenId)
     globalActions.setIdToken(response.tokenId)
     globalActions.setApiClient()
     globalActions.setIsSignedIn(true)
